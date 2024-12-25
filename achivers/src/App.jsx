80d12e67c72b215a7da1c1e76d62e74a3b4f9bca
Nitesh <niteshgurav098765@gmail.com>
2024-12-25 23:48:12 +0530

    initial-commit

---
+++ achivers/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Nav from './Navbar'
+import acfImg from './assets/image.png';
+
+
+function App() {
+
+
+  return (
+    <>
+    <Nav></Nav>
+    <img src={acfImg} alt="" />
+    </>
+  )
+}
+
+export default App
